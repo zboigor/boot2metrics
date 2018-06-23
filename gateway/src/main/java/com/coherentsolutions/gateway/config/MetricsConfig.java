c0c65b32dbd9670ceab0845f21b238ba65ca6f91
Igor Zboichik <zboigor@gmail.com>
2018-06-24 00:26:48 +0300

    Fixed uri for metrics in zuul

--- gateway/src/main/java/com/coherentsolutions/gateway/config/MetricsConfig.java
+++ gateway/src/main/java/com/coherentsolutions/gateway/config/MetricsConfig.java
@@ -1,8 +1,16 @@
 package com.coherentsolutions.gateway.config;
 
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.config.MeterFilter;
+import org.springframework.boot.actuate.metrics.web.servlet.DefaultWebMvcTagsProvider;
+import org.springframework.boot.actuate.metrics.web.servlet.WebMvcTags;
+import org.springframework.boot.actuate.metrics.web.servlet.WebMvcTagsProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class MetricsConfig {
@@ -13,5 +21,16 @@ public class MetricsConfig {
             String uri = id.getTag("uri");
             return uri != null && (uri.startsWith("/actuator") || uri.contains("favicon.ico"));
         });
+    }
+
+    @SuppressWarnings("unchecked")
+    @Bean
+    public WebMvcTagsProvider webMvcTagsProvider() {
+        return new DefaultWebMvcTagsProvider() {
+            public Iterable<Tag> getTags(HttpServletRequest request, HttpServletResponse response, Object handler, Throwable exception) {
+                Tag uriTag = Tag.of("uri", String.valueOf(request.getRequestURI()));
+                return Tags.of(WebMvcTags.method(request), uriTag, WebMvcTags.exception(exception), WebMvcTags.status(response));
+            }
+        };
     }
 }
