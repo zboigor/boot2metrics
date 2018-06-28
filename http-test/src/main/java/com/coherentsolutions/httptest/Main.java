8c6bb13132657326b6ba78ebfdfdd16d7655d965
Igor Zboichik <zboigor@gmail.com>
2018-06-28 10:30:18 +0300

    Run for meetup

--- http-test/src/main/java/com/coherentsolutions/httptest/Main.java
+++ http-test/src/main/java/com/coherentsolutions/httptest/Main.java
@@ -3,9 +3,25 @@ package com.coherentsolutions.httptest;
 public class Main {
 
     public static void main(String[] args) {
-        ApiExecutor executor = new ApiExecutor(4);
-//        executor.execute(50, 2);
-        executor.execute(10000);
+        ApiExecutor executor = new ApiExecutor(32);
+
+        // 1
+//        executor.execute(32, 5);
+//        executor.execute(32, 2);
+
+        // 2
+//        executor.execute(64);
+
+        // 3
+//        executor.execute(32, 1);
+//        executor.execute(32, 4);
+//        executor.execute(32, 8);
+
+        // 4
+//        executor.execute(32, 2);
+//        executor.execute(32);
+
+//        executor.execute(32 * 8, 1);
 
         executor.shutdown();
     }
